2d37c94eda6560e15a849f27ebc0b85529d3d08d
khushbu vohora <gulnaz328@gmail.com>
2025-01-21 16:29:03 +0000

    landing page-features

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,11 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+console.log("Connected")
+const btn = document.getElementById("click-me")
+console.log(btn)
+btn.addEventListener("click", () => {
+  const audio = new Audio("./Evil_laugh.mp3");
+  audio.play();
+});
