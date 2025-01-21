43bb6ec383ba89450a9ff627320552b7e9461691
lucas halfpenny <lucashalfpenny@gmail.com>
2025-01-21 15:13:41 +0000

    Changed user associations and created seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :lairs, dependent: :destroy
-  has_many :lairs,  through: :bookings
+
+  # Owned lairs
+  has_many :lairs, foreign_key: :owner_id, class_name: "Lair"
+
+  # Rented lairs
+  has_many :bookings
+  has_many :rented_lairs, through: :bookings, source: :lair
+
+  # has_many :lairs, dependent: :destroy, class_name: "Lair"
+  # has_many :bookings
+  # has_many :lairs, through: :bookings, source: :lair
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
