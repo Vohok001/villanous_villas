04a1c898d9fb511c310f47135d313a93385a84dd
Richard Carveth <richardcarveth191@hotmail.com>
2025-01-22 11:57:39 +0000

    Added Date conversion for checkin/out to help validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,6 +10,10 @@ class Booking < ApplicationRecord
   def check_out_after_check_in
     return if check_out.blank? || check_in.blank?
 
+    begin
+      check_in = Date.parse(check_in.to_s)
+      check_out = Date.parse(check_out.to_s)
+
     if check_out <= check_in
       errors.add(:check_out, "must be after the check-in date")
     end
